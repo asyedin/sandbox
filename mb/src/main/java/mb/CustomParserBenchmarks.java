828be42b765058e5fe8e8ae42e74b1e826ccd013
alex <Alex Syedin>
2015-12-16 21:53:56 +0100

    cleaning up unused stuff

--- mb/src/main/java/mb/CustomParserBenchmarks.java
+++ mb/src/main/java/mb/CustomParserBenchmarks.java
@@ -26,14 +26,12 @@ public class CustomParserBenchmarks {
 
     @Setup(Level.Iteration)
     public void init() {
-        System.err.println("setup");
         asString = String.valueOf(RND.nextLong());
     }
 
 
     @Benchmark
     public long longParselong() {
-
         long result = Long.parseLong(asString);
         return result;
     }
@@ -42,30 +40,6 @@ public class CustomParserBenchmarks {
     public long customParseLong() {
         long result = CustomParser.parseLong(asString);
         return result;
-    }
-
-    public static void main1(String... args) throws RunnerException {
-        PrintWriter pw = new PrintWriter(System.out, true);
-        for (int b = 1; b <= 1048576; b *= 2) {
-            for (int t = 1; t <= Runtime.getRuntime().availableProcessors(); t++) {
-                runWith(pw, t, b);
-            }
-        }
-    }
-
-    private static void runWith(PrintWriter pw, int threads, int backoff) throws RunnerException {
-        Options opts = new OptionsBuilder()
-//                .include(".*" + CustomParserBenchmarks.class.getName() + ".*")
-                .include(CustomParserBenchmarks.class.getSimpleName())
-                .threads(threads)
-                .verbosity(VerboseMode.SILENT)
-                .param("backoff", String.valueOf(backoff))
-                .build();
-
-        RunResult r = new Runner(opts).runSingle();
-        double score = r.getPrimaryResult().getScore();
-        double scoreError = r.getPrimaryResult().getStatistics().getMeanErrorAt(0.99);
-        pw.printf("%6d, %3d, %11.3f, %10.3f%n", backoff, threads, score, scoreError);
     }
 
     public static void main(String[] args) throws RunnerException {
